fad24176e506509b0114a33f672cc980ea8c768d
ParkJaeHwan <doormoo2@naver.com>
2025-09-04 13:02:11 +0900

    pjt init

---
+++ backend/src/test/java/com/ygss/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygss.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
