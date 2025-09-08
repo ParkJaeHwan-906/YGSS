32fe97bb511fd63f35adc8746cc3fd27d452b421
rlaqkaql1012 <rlaqkaql1012@naver.com>
2025-09-08 17:58:15 +0900

    feat : entities

--- backend/src/main/java/com/ygss/backend/pensionProduct/dto/entity/PensionProduct.java
+++ backend/src/main/java/com/ygss/backend/pensionProduct/dto/entity/PensionProduct.java
@@ -1,4 +1,31 @@
 package com.ygss.backend.pensionProduct.dto.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PensionProduct {
+    private Long id;
+    private Long companyId;
+    private Long systypeId;
+    private Long productTypeId;
+    private String product;           // 상품명
+    private Integer riskGrade;
+    private Long reserve;          // 운용자산
+    private Double nextYearProfitRate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // 조인된 정보
+    private String companyName;
+    private String productTypeName;
+    private String systypeName;
+
 }
--- backend/src/main/java/com/ygss/backend/pensionProduct/dto/entity/ProductType.java
+++ backend/src/main/java/com/ygss/backend/pensionProduct/dto/entity/ProductType.java
@@ -1,4 +1,19 @@
 package com.ygss.backend.pensionProduct.dto.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ProductType {
+    private Long id;
+    private String productType;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
