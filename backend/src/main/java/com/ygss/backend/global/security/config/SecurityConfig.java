fe2af452c2493b2bb32104b974bf2cf92ec5163e
ParkJaeHwan <doormoo2@naver.com>
2025-09-12 17:40:35 +0900

    FIX : Add Route Product API PermitAll

--- backend/src/main/java/com/ygss/backend/global/security/config/SecurityConfig.java
+++ backend/src/main/java/com/ygss/backend/global/security/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
 //                        .requestMatchers("/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/infra/**").permitAll()
+                        .requestMatchers("/product/**").permitAll()
                         .requestMatchers("/investor/personality/list").permitAll()
                         .requestMatchers("/recommend/public/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/api-docs/**", "/swagger-ui.html").permitAll()
