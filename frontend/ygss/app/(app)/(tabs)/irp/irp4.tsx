b383b53d067479fa761e55317e4d8eb6c8bd09a2
최유진 <ygg1202@naver.com>
2025-09-18 15:05:17 +0900

    Feat: irp 페이지 연동

---
+++ frontend/ygss/app/(app)/(tabs)/irp/irp4.tsx
@@ -0,0 +1,12 @@
+// app/(app)/(tabs)/irp/irp4.tsx
+
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Irp4() {
+    return (
+        <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>계좌 추천 페이지</Text>
+        </SafeAreaView>
+    );
+}
