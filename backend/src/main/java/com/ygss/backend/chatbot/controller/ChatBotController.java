54554a5deadcb26218208c5df88ecbc9be21f4c7
ParkJaeHwan <doormoo2@naver.com>
2025-10-02 15:05:08 +0900

    roll back

--- backend/src/main/java/com/ygss/backend/chatbot/controller/ChatBotController.java
+++ backend/src/main/java/com/ygss/backend/chatbot/controller/ChatBotController.java
@@ -19,19 +19,7 @@ public class ChatBotController {
     @PostMapping({"/send/", "/send/{sid}"})
     public ResponseEntity<?> sendToChatBot(@RequestBody SendChatRequestDto request, @PathVariable(required = false) String sid) {
         try {
-//            return ResponseEntity.ok(chatBotService.requestAnswer(request, sid));
-            Thread.sleep(1300);
-            return ResponseEntity.ok(ChatBotResponseDto.builder()
-                            .sid(sid)
-                            .answer("""
-                                    DB, DC, IRP 각 차이점에 대해서 설명해드릴게요!
-                                    DB는 회사가 운용 책임, 퇴직 시 받을 금액이 🛡️확정되어 있어요. (안정적)
-                                    
-                                    DC는 내가 직접 굴려 수익을 내는 🚀투자형이에요. 운용 성과에 따라 최종 금액이 달라져요.
-                                    
-                                    IRP는 퇴직금 등을 모아 내가 운용하는 ✨개인 전용 계좌예요. 세액공제 혜택이 가장 커요.
-                                    """)
-                    .build());
+            return ResponseEntity.ok(chatBotService.requestAnswer(request, sid));
         } catch (Exception e) {
             log.error("Send To ChatBot Failed : {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(false);
